fcffee56a614e43c121731218ba6619bff3ccd71
hasheem-tech <hasheemnaveed19@gmail.com>
2026-04-13 02:15:42 +0500

    fixed some errors

--- project/scripts/contact.js
+++ project/scripts/contact.js
@@ -3,21 +3,3 @@ window.onload = function () {
 };
 
 
-const outputDiv = document.getElementById("output");
-
-// get query string from URL
-const params = new URLSearchParams(window.location.search);
-
-// extract values
-const name = params.get("name");
-const email = params.get("email");
-const purpose = params.get("select");
-const message = params.get("message");
-
-// display in div
-outputDiv.innerHTML = `
-  <p><strong>Name:</strong> ${name}</p>
-  <p><strong>Email:</strong> ${email}</p>
-  <p><strong>Purpose:</strong> ${purpose}</p>
-  <p><strong>Message:</strong> ${message}</p>
-`;
---
+++ project/scripts/thankyou.js
@@ -0,0 +1,18 @@
+const outputDiv = document.getElementById("output");
+
+// get query string from URL
+const params = new URLSearchParams(window.location.search);
+
+// extract values
+const name = params.get("name");
+const email = params.get("email");
+const purpose = params.get("select");
+const message = params.get("message");
+
+// display in div
+outputDiv.innerHTML = `
+  <p><strong>Name:</strong> ${name}</p>
+  <p><strong>Email:</strong> ${email}</p>
+  <p><strong>Purpose:</strong> ${purpose}</p>
+  <p><strong>Message:</strong> ${message}</p>
+`;
